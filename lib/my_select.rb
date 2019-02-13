a011ad27e46edd4a874256f42683a40155f755db
Tao Xu <nytaoxu@gmail.com>
2019-02-13 23:17:15 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,19 @@
 def my_select(collection)
- # your code here!
+# # your code here!
+# x = 10
+# y = yield x
+# puts y.class
+# yield returns whatever the code block returns
+  
+  result = Array.new
+  index = 0
+  while index < collection.length
+    if yield(collection[index])
+      result << collection[index]
+    end
+    index += 1
+  end
+  result
 end
+
+# my_select(123) {puts 123}
